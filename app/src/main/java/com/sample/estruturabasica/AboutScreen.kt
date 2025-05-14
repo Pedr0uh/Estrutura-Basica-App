d53402fcb2fb2ccfb7cedb92aaf28816155df35a
Pedro Henrique <ppedrouh@gmail.com>
2025-05-14 11:58:07 -0300

    Updates

--- app/src/main/java/com/sample/estruturabasica/AboutScreen.kt
+++ app/src/main/java/com/sample/estruturabasica/AboutScreen.kt
@@ -1,9 +1,13 @@
 package com.sample.estruturabasica
 
-import android.service.autofill.OnClickAction
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -13,16 +17,19 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.sample.estruturabasica.ui.theme.Purple80
 
 @Composable
 fun AboutScreen(navController: NavController) {
@@ -36,25 +43,39 @@ fun AboutScreen(navController: NavController) {
             )
         },
         bottomBar = {
-            BottomAppBar {
-                BottomNavItem(
-                    icon = Icons.Default.Info,
-                    label = "Sobre",
-                    isSelected = true,
-                    onClick = { /* ja esta na sobre*/ }
-                )
-                BottomNavItem(
-                    icon = Icons.Default.Home,
-                    label = "Home",
-                    isSelected = false,
-                    onClick = { navController.navigate(Screen.Home.route) }
-                )
-                BottomNavItem(
-                    icon = Icons.Default.Email,
-                    label = "Contato",
-                    isSelected = false,
-                    onClick = { navController.navigate(Screen.Contact.route) }
-                )
+            BottomAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 60.dp, vertical = 25.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .height(70.dp),
+                containerColor = Purple80
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    BottomNavItem(
+                        icon = Icons.Default.Info,
+                        label = "Sobre",
+                        isSelected = true,
+                        onClick = { /* ja esta na sobre*/ }
+                    )
+                    BottomNavItem(
+                        icon = Icons.Default.Home,
+                        label = "Home",
+                        isSelected = false,
+                        onClick = { navController.navigate(Screen.Home.route) }
+                    )
+                    BottomNavItem(
+                        icon = Icons.Default.Email,
+                        label = "Contato",
+                        isSelected = false,
+                        onClick = { navController.navigate(Screen.Contact.route) }
+                    )
+                }
+
             }
         },
         floatingActionButton = {
--- app/src/main/java/com/sample/estruturabasica/HomeScreen.kt
+++ app/src/main/java/com/sample/estruturabasica/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.sample.estruturabasica
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -17,6 +21,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -28,6 +33,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sample.estruturabasica.R
+import com.sample.estruturabasica.ui.theme.Purple80
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -43,30 +49,36 @@ fun HomeScreen(navController: NavController) {
             BottomAppBar(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 30.dp, vertical = 30.dp)
+                    .padding(horizontal = 70.dp, vertical = 10.dp)
                     .clip(RoundedCornerShape(20.dp))
                     .background(Color.Black)
                     .height(70.dp),
-                containerColor = Color.LightGray
+                containerColor = Purple80
             ) {
-                BottomNavItem(
-                    icon = Icons.Default.Home,
-                    label = "Home",
-                    isSelected = true,
-                    onClick = { /* Já estamos na home */ }
-                )
-                BottomNavItem(
-                    icon = Icons.Default.Info,
-                    label = "Sobre",
-                    isSelected = false,
-                    onClick = { navController.navigate(Screen.About.route) }
-                )
-                BottomNavItem(
-                    icon = Icons.Default.Email,
-                    label = "Contato",
-                    isSelected = false,
-                    onClick = { navController.navigate(Screen.Contact.route) }
-                )
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    BottomNavItem(
+                        icon = Icons.Default.Home,
+                        label = "Home",
+                        isSelected = true,
+                        onClick = { /* Já estamos na home */ }
+                    )
+                    BottomNavItem(
+                        icon = Icons.Default.Info,
+                        label = "Sobre",
+                        isSelected = false,
+                        onClick = { navController.navigate(Screen.About.route) }
+                    )
+                    BottomNavItem(
+                        icon = Icons.Default.Email,
+                        label = "Contato",
+                        isSelected = false,
+                        onClick = { navController.navigate(Screen.Contact.route) }
+                    )
+                }
             }
         }
     ) { paddingValues ->
